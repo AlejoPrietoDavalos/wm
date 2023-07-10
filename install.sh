83b22451e0b226df8f3ecf51704a6e553bf83ec5
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-07-10 19:29:09 -0300

    Picom para transparencias de la terminal y polybar

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ main_dir=$(realpath $(dirname $0))
 chmod +x $main_dir/bspwm/*
 chmod +x $main_dir/sxhkd/*
 chmod +x $main_dir/polybar/launch.sh
-cp -r $main_dir/{bspwm,sxhkd,polybar} ~/.config
+chmod +x $main_dir/picom/*
+cp -r $main_dir/{bspwm,sxhkd,polybar,picom} ~/.config
 
 # Reiniciamos bspwm
 bspc wm -r
