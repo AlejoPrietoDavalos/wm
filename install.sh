bbd8d0d93b1fca5474fdfd242ae2d84c496c1a24
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-07-10 22:01:58 -0300

    se crea la carpeta config, donde van todas las carpetas que van a ser enviadas a /home/pepocho/.config

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-main_dir=$(realpath $(dirname $0))
-chmod +x $main_dir/bspwm/*
-chmod +x $main_dir/sxhkd/*
-chmod +x $main_dir/polybar/launch.sh
-chmod +x $main_dir/picom/*
-chmod +x $main_dir/kitty/*
-cp -r $main_dir/{bspwm,sxhkd,polybar,picom,kitty} ~/.config
 
+path_config=$PWD/config
+for path_conf_i in $(ls -d $path_config/*); do
+    conf_name=`basename $path_conf_i`
+    chmod +x $path_conf_i/*
+    cp -r $path_conf_i/* $HOME/.config/$conf_name
+done
 # Reiniciamos bspwm y sxhkd
 bspc wm -r
 pkill -USR1 -x sxhkd
