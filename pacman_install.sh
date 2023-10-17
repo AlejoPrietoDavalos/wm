969accd8b279ec257a4cef7a0ce5e8f22ad4f7e2
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-17 22:08:46 +0000

    pacman instalación de paquetes

---
+++ pacman_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo pacman -S htop cmatrix discord spotify-launcher ntfs-3g pulseaudio
+
+
+#sudo pacman -S nano git python3 bspwm sxhkd polybar dmenu rofi picom feh scrot kitty
+#sudo pacman -S xorg xorg-xinit xorg-xserver xorg-xrandr
+#sudo pacman -S firefox
+
+# Dentro de /etc/pacman.conf descomentar Include = /etc/pacmand.d/mirrorlist.
+#sudo pacman -Sy
+#sudo pacman -S nvidia
+
+# Ejecutar los siguiente comandos (Mencionar los de GPT).
+#Xorg:0 -configure  # Esto va a crear un fichero xorg.conf.new en $HOME.
+#cp ~/xorg.conf.new /etc/X11/xorg.conf
+#echo "exec bspwm" >> ~/.xinitrc
+
+
+
+
+#sudo pacman -S lightdm
+#sudo pacman -S lightdm-gtk-greeter
+# Ejecutar: sudo systemctl enable lightdm
