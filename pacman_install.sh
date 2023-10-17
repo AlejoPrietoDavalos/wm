c7093eb542b3940eef50c95f1b98f160140fd0b9
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-17 22:27:47 +0000

    install

--- pacman_install.sh
+++ pacman_install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-sudo pacman -S htop cmatrix discord spotify-launcher ntfs-3g pulseaudio
+#sudo pacman -S htop cmatrix discord spotify-launcher ntfs-3g pulseaudio
+#sudo pacman -S nano git python3 bspwm sxhkd polybar dmenu rofi picom feh scrot kitty firefox
+#sudo pacman -S xorg xorg-xinit xorg-xserver xorg-xrandr lightdm lightdm-gtk-greeter
+#sudo pacman -S lightdm lightdm-gtk-greeter
 
-
-#sudo pacman -S nano git python3 bspwm sxhkd polybar dmenu rofi picom feh scrot kitty
-#sudo pacman -S xorg xorg-xinit xorg-xserver xorg-xrandr
-#sudo pacman -S firefox
 
 # Dentro de /etc/pacman.conf descomentar Include = /etc/pacmand.d/mirrorlist.
 #sudo pacman -Sy
 #sudo pacman -S nvidia
 
+
 # Ejecutar los siguiente comandos (Mencionar los de GPT).
 #Xorg:0 -configure  # Esto va a crear un fichero xorg.conf.new en $HOME.
+# Hay que copiarlo y mandarlo a este path:
 #cp ~/xorg.conf.new /etc/X11/xorg.conf
 #echo "exec bspwm" >> ~/.xinitrc
 
 
 
 
-#sudo pacman -S lightdm
-#sudo pacman -S lightdm-gtk-greeter
+
+
+
 # Ejecutar: sudo systemctl enable lightdm
