2851f2f46563b1e366f885dcaacf970641443bb9
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-29 19:07:36 -0300

    asd

--- commands.sh
+++ commands.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install snapd
-sudo snap install discord -y
-sudo snap install spotify -y
+#git config --global credential.helper store
 
